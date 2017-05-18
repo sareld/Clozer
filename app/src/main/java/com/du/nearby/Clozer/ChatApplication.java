00941ef84943aec091ecd318e77e4e18659ec2b7
bareluz93 <bareluz93@gmail.com>
2017-05-18 22:34:21 +0300

    with chat not runnig

---
+++ app/src/main/java/com/du/nearby/Clozer/ChatApplication.java
@@ -0,0 +1,28 @@
+package com.du.nearby.Clozer;
+
+import android.app.Application;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+
+import java.net.URISyntaxException;
+
+public class ChatApplication extends Application {
+
+    private Socket mSocket;
+    {
+        try {
+            IO.Options options = new IO.Options();
+            options.forceNew=true;
+            options.reconnection = false;
+            options.query = "room_var=39";
+            mSocket = IO.socket(Constants.CHAT_SERVER_URL,options);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Socket getSocket() {
+        return mSocket;
+    }
+}
+
---
+++ app/src/main/java/com/du/nearby/Clozer/Constants.java
@@ -0,0 +1,5 @@
+package com.du.nearby.Clozer;
+
+public class Constants {
+    public static final String CHAT_SERVER_URL = "https://relic-pie.glitch.me/";
+}
