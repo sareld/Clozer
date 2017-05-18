47561ee412c893ffe155aca3961d375d7845386c
Sarel <sarel.duanis@mail.huji.ac.il>
2017-05-18 18:30:54 +0300

    clozer change

--- app/src/main/java/com/du/nearby/Clozer/MainActivity.java
+++ app/src/main/java/com/du/nearby/Clozer/MainActivity.java
@@ -36,73 +36,13 @@ public class MainActivity extends FragmentActivity{
 
         Profile profile = Profile.getCurrentProfile();
 
-        if(profile != null) {
+        Log.d(TAG, "setContentView" );
 
-            mainFragment = new mainFragment();
+        loginFragment = new loginFragment();
 
-            getSupportFragmentManager().beginTransaction()
-                    .add(android.R.id.content, mainFragment).commit();
-        }else {
-            Log.d(TAG, "setContentView" );
-
-            loginFragment = new loginFragment();
-
-            getSupportFragmentManager().beginTransaction()
+        getSupportFragmentManager().beginTransaction()
                     .add(android.R.id.content, loginFragment).commit();
 
-        /*
-            final ListView lstView = (ListView) findViewById(R.id.listView);
-            final Button AddGroupButton = (Button) findViewById(R.id.button);
-            final EditText msgTxt = (EditText) findViewById(R.id.editText);
-            Messages = new ArrayList<String>();
-            Messages.add("asdfasdf");
-            final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_view, Messages);
-            lstView.setAdapter(adapter);
-
-            AddGroupButton.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    String msg = msgTxt.getText().toString();
-                    Log.d(TAG, msg);
-                    publish(msg);
-                    Messages.add(msg);
-                    adapter.notifyDataSetChanged();
-                }
-            });
-
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                    == PackageManager.PERMISSION_GRANTED) {
-                mGoogleApiClient = new GoogleApiClient.Builder(this)
-                        .addApi(Nearby.MESSAGES_API, new MessagesOptions.Builder()
-                                .setPermissions(NearbyPermissions.BLE)
-                                .build())
-                        .addConnectionCallbacks(this)
-                        .enableAutoManage(this, this)
-                        .build();
-            } else {
-                mGoogleApiClient = new GoogleApiClient.Builder(this)
-                        .addApi(Nearby.MESSAGES_API)
-                        .addConnectionCallbacks(this)
-                        .enableAutoManage(this, this)
-                        .build();
-            }
-
-            mMessageListener = new MessageListener() {
-                @Override
-                public void onFound(Message message) {
-                    String messageAsString = new String(message.getContent());
-                    Log.d(TAG, "Found message: " + messageAsString);
-                    Messages.add(messageAsString);
-                    adapter.notifyDataSetChanged();
-                }
-
-                @Override
-                public void onLost(Message message) {
-                    String messageAsString = new String(message.getContent());
-                    Log.d(TAG, "Lost sight of message: " + messageAsString);
-                }
-            };
-            Log.i(TAG, "Finished create ");*/
-        }
 
     }
 
